c77399f75442afa356bbefddc53f3c5368cfe737
SenaUstun-Dev <senaustun.dev@gmail.com>
2025-02-26 10:16:10 +0300

    Config and exception package added

---
+++ src/main/java/com/staj/tasklist/expections/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.staj.tasklist.expections;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+/*
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+*/
+public class GlobalExceptionHandler {
+    //will be added later
+//    @ExceptionHandler(expectionName.class)
+//    public ResponseEntity<?>exceptionName(){
+//        ...
+//        return new ResponseEntity<>("Something went wrong", HttpStatus.INTERNAL_SERVER_ERROR);
+//    }
+}
